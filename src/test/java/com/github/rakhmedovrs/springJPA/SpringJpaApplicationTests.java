0ca4d39d53fbcd0136f2b0cc22f2431cba45b298
rassoll <rassoll32@gmail.com>
2020-04-13 22:27:36 +0300

    Initial commit

---
+++ src/test/java/com/github/rakhmedovrs/springJPA/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rakhmedovrs.springJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
